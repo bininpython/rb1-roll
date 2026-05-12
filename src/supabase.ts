943c135e140456e2c506749e9df81097a726d606
Abner <abnerlucas571@gmail.com>
2026-05-12 02:34:50 -0300

    fix: atualizacao da chave anon publica do supabase para acesso correto ao banco de dados

--- src/supabase.ts
+++ src/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Usando o URL extraído da sua string de conexão Postgres
 const supabaseUrl = 'https://ftxexhdigckykgwcurzx.supabase.co';
-// Usando a chave pública que você informou
-const supabaseKey = 'sb_publishable_uISv5tu4ciytCpoSWvXmAg_KT3T2cjM';
+// Usando a chave pública (anon) correta
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0eGV4aGRpZ2NreWtnd2N1cnp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1NDUzNjEsImV4cCI6MjA5NDEyMTM2MX0.P5H3-1FvAeXJFeFOrfHIXxQGHt29raiYoe1tj82YIvA';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
